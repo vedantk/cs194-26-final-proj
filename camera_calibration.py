abe2266310aa1ae0834bec5f9eac046ba052761a
Melanie Cebula <melaniecebula@berkeley.edu>
2014-12-06 15:32:30 -0800

    cleanup

--- camera_calibration.py
+++ camera_calibration.py
@@ -13,9 +13,8 @@ objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
+#Grab all jpg images in this directory (currently all sample chess images)
 images = glob.glob('*.jpg')
-print "hi"
-print images
 
 for fname in images:
     img = cv2.imread(fname)
@@ -31,7 +30,7 @@ for fname in images:
         cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
         imgpoints.append(corners)
 
-        #Melanie note: changed corners2 to corners- supposed to be output of findChessboardCorners
+        #Melanie note: corners is output of findChessboardCorners
         # Draw and display the corners
         cv2.drawChessboardCorners(img, (7,6), corners,ret)
         cv2.imshow('img',img)
@@ -48,7 +47,8 @@ for fname in images:
         h,  w = img.shape[:2]
         newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
 
-        #Method 1.  cv2.undistort()
+        #Method 1.  cv2.undistort() 
+        #Melanie note:  there is also the remapping method, but the output is the same and this is less hairy
         # undistort
         dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
 
@@ -57,27 +57,14 @@ for fname in images:
         dst = dst[y:y+h, x:x+w]
         cv2.imwrite('calibresult.png',dst)
 
-        #Method 2.  remapping
-        # undistort
-        #mapx,mapy = cv2.initUndistortRectifyMap(mtx,dist,None,newcameramtx,(w,h),5)
-        #dst = cv2.remap(img,mapx,mapy,cv2.INTER_LINEAR)
-
-        # crop the image
-        #x,y,w,h = roi
-        #dst = dst[y:y+h, x:x+w]
-        #cv2.imwrite('calibresult.png',dst)
-
-        #Melanie:  looks like they both get the same result- method 1 looks easier to code
         #Re-projection error:
         mean_error = 0
         for i in xrange(len(objpoints)):
             imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
             error = cv2.norm(imgpoints[i],imgpoints2, cv2.NORM_L2)/len(imgpoints2)
-            #Melanie:  s/tot_error/mean_error --> should be close to 0
             mean_error += error
 
+        #Melanie note:  goal is for this to be as close to 0 as possible
         print "total error: ", mean_error/len(objpoints)
-
-
 
 cv2.destroyAllWindows()
