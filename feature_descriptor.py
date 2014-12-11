3f9c11bd4310855c0da0936ad17e3e35b9400c15
Siddhartho Bhattacharya <sid_gogol@hotmail.com>
2014-12-10 18:56:51 -0800

    competed find features for MOPS

--- feature_descriptor.py
+++ feature_descriptor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python2
 
+import cv2
 from collections import namedtuple
 
 # FeatureDescriptor:
@@ -24,7 +25,9 @@ def find_features(img, method='SIFT', nfeatures=1000):
         kp, des = sift.detectAndCompute(img, None)
 
     elif method == 'MOPS':
-        pass
+        keyPts = ANMS(im, nfeatures) 
+        patches, coords = preparePatches(im, ketPts)
+        return FeatureDescriptor(coords, patches)
 
 def find_matches(features1, features2, method='SIFT', nmatches=1000):
     '''
@@ -36,3 +39,77 @@ def find_matches(features1, features2, method='SIFT', nmatches=1000):
         correspondences: a list of [(pt1, pt2), ...]
     '''
     pass
+
+######################
+## <HELPER METHODS> ##
+######################
+def blockMax(im, blockSize=3):
+  filtered = np.zeros(im.shape).astype("bool")
+  #filtered = im.copy()
+  for r in xrange(im.shape[0]-blockSize+1):
+    for c in xrange(im.shape[1]-blockSize+1):
+      block = im[r:r+blockSize, c:c+blockSize]
+      filtered[r:r+blockSize, c:c+blockSize] |= block == np.max(block)
+  return im*(filtered.astype(float))
+
+def findHarris(im, blockSize=3, ksize=3, k=0.04, edgeWidth=20):
+  gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY).astype("float32")
+  corners = cv2.cornerHarris(gray, blockSize, ksize, k)
+  dst = blockMax(corners, 3)
+  dst[:edgeWidth,:] = 0.0
+  dst[im.shape[0]-edgeWidth:,:] = 0.0
+  dst[:,:edgeWidth] = 0.0
+  dst[:,im.shape[1]-edgeWidth:] = 0.0
+  return dst
+
+def maxCoords(arr, n):
+  topr, topc = np.unravel_index(arr.ravel().argsort()[::-1][:n], arr.shape)
+  topCoords = np.zeros(arr.shape)
+  topCoords[topr, topc] = arr[topr, topc]
+  return topCoords
+
+def preparePatches(im, pts, blockSize=20, sampleRate=5):
+  gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+  numr, numc = pts.shape
+  r,c = np.nonzero(pts)
+  coords = np.array(zip(r,c))
+  blurred = cv2.GaussianBlur(gray, (0,0), 1)
+  mat = []
+  for coord in coords:
+    row, col = coord
+    top = max(0, row-blockSize)
+    bot = min(numr-1, row+blockSize)
+    left = max(0, col-blockSize)
+    right = min(numc-1, col+blockSize)
+    box = blurred[top:bot, left:right]
+
+    sub = box[::sampleRate, ::sampleRate].flatten().astype(float)
+    sub = (sub-np.mean(sub))/np.std(sub)
+    mat.append(sub)
+  return np.array(mat), coords
+
+def ANMS(im, n, crobust=0.9):
+  """ Does Adaptive Non-Maximal Supression to
+  Obtain N Key Points from Harris Corners
+  """
+  corners = findHarris(im)
+  numr, numc = corners.shape
+  radii = np.zeros(corners.shape)
+  r,c = np.nonzero(corners)
+  coords = np.array(zip(r,c))
+  for coord in coords:
+    row, col = coord
+    for radius in range(1, max(numr, numc)):
+      top = max(0, row-radius)
+      bot = min(numr-1, row+radius+1)
+      left = max(0, col-radius)
+      right = min(numc-1, col+radius+1)
+      box = corners[top:bot, left:right]
+      if corners[row,col] < crobust*(np.max(box)):
+        radii[row,col] = radius
+        break
+  return maxCoords(radii, n)
+
+#######################
+## </HELPER METHODS> ##
+#######################
