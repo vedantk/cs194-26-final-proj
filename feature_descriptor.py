b539a84f3b35c14ebac9b59c3e6c2594dd553f9b
Vedant Kumar <vk@vedantk.com>
2014-12-10 18:35:06 -0800

    Add feature_descriptor.py

---
+++ feature_descriptor.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python2
+
+from collections import namedtuple
+
+FeatureDescriptor = namedtuple('FeatureDescriptor', 'pos desc')
+
+def find_features(img, method='SIFT', nfeatures=1000):
+    '''
+    Input:
+        img: a cv2 image (color images are supported)
+        method: may be 'SIFT' or 'MOPS'
+    Output:
+        features: a list of FeatureDescriptor objects
+    '''
+
+    # Convert the image to grayscale if necessary.
+    im = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) if len(img.shape) > 2 else img
+
+    if method == 'SIFT':
+        sift = cv2.SIFT(nfeatures)
+        kp, des = sift.detectAndCompute(img, None)
+    elif method == 'MOPS':
+        pass
+
+def find_matches(features1, features2, method='SIFT', nmatches=1000):
+    '''
+    Input:
+        features1, features2: list of FeatureDescriptor
+        method: may be 'SIFT' or 'MOPS'
+        nmatches: maximum number of correspondence points to find
+    Output:
+        correspondences: a list of [(pt1, pt2), ...]
+    '''
+    pass
