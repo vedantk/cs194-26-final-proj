ca0d97c693882f1d45080cc61baf6c970f47c6f1
Melanie Cebula <melaniecebula@berkeley.edu>
2014-12-10 23:52:00 -0800

    before pull

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import feature_descriptor
 import cv2
+import math
 from pprint import pprint
 
 if __name__ == "__main__":
@@ -12,14 +13,19 @@ if __name__ == "__main__":
 
     json_filename = args.filename
     with open(json_filename) as f:
+        #tuples of image names and camera pose (theta, phi, calibration matrix)
         data = json.load(f)
         #uncomment to print json data format
-        #print(data)
+        #pprint(data)
 
         images = [cv2.imread(image) for image, _ in data]
-        #Using SIFT by default
-        print images
+        #NOTE: Using SIFT by default
         features = [feature_descriptor.find_features(image) for image in images]
+
+        #Find all corr. points between adjacent pairs of images
+        corr_pts = []
+        theta = math.pi/2
+        phi = math.pi/2
         for i in range(1, len(features) - 1):
-            feature_descriptor.find_matches(features[i], features[i + 1])
+            corr_pts.append(feature_descriptor.find_matches(features[i], features[i + 1]))
 
